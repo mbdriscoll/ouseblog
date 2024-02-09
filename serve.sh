9f5b9e418c5b8759be9a55bcf714b2aa77d44d92
Michael Driscoll <mbdriscoll@gmail.com>
2024-02-09 18:54:47 -0500

    serve script and gitignore

---
+++ serve.sh
@@ -0,0 +1,12 @@
+docker run \
+  --name ouseblog-dev-server \
+  -u $(id -u):$(id -g) \
+  -p 1313:1313 \
+  -v $(pwd)/site:/site \
+  hugo server \
+    --source /site \
+    --bind 0.0.0.0 \
+    --port 1313 \
+    --theme etch \
+    --watch \
+    --disableFastRender
