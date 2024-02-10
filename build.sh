0eee97831aefa2874013548dfea356f403276071
Michael Driscoll <mbdriscoll@gmail.com>
2024-02-09 19:16:10 -0500

    Update build, entrypoint, and serve scripts to support ctrl-c exit on server.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-docker build -t hugo .
+docker build -t ouseblog .
--- serve.sh
+++ serve.sh
@@ -1,8 +1,12 @@
+#!/bin/bash -ex
+
 docker run \
   --name ouseblog-dev-server \
   -u $(id -u):$(id -g) \
   -p 1313:1313 \
   -v $(pwd)/site:/site \
+  --rm \
+  ouseblog \
   hugo server \
     --source /site \
     --bind 0.0.0.0 \
